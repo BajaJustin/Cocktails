b861202ffa8495dd536b61c060cacf6b811689d9
BajaJustin <justindabaja@gmail.com>
2020-11-02 13:49:20 -0500

    Finished tutorial

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi vero architecto adipisci reiciendis repudiandae officiis dignissimos eius autem cumque quos fugit aliquam praesentium obcaecati sed ipsam laborum, laboriosam ipsa dolor dicta! Aliquam non praesentium totam omnis magnam, a facilis aut.</p>
+    </section>
+  )
+}
+
+export default About
